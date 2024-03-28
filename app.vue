e441d446ac63d5755d1db306a774f097cd411adb
Vien Huynh <vien200203@gmail.com>
2024-03-28 11:13:26 +0700

    feat(auth): login page and modal logout

--- app.vue
+++ app.vue
@@ -5,6 +5,11 @@
       { 'max-h-[100vh] transition-all duration-200 ease-in visible': userStore.isMenuOverlay },
       { 'max-h-0 transition-all duration-200 ease-out invisible': !userStore.isMenuOverlay },
     ]" />
+    <Modal :class="[
+      { 'max-h-[100vh] transition-all duration-200 ease-in visible': userStore.isLogoutOverlay },
+      { 'max-h-0 transition-all duration-200 ease-out invisible': !userStore.isLogoutOverlay },
+    ]" />
+
 
   </div>
 </template>
